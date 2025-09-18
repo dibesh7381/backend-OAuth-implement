8c6158a623f1528d6937ae9b286a3e738256d7f3
Dibesh Ranjan <149006547+dibesh7381@users.noreply.github.com>
2025-09-19 02:30:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const User = mongoose.model("User", userSchema);
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: " https://backend-oauth-implement.onrender.com/auth/google/callback"
+    callbackURL: "https://backend-oauth-implement.onrender.com/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
